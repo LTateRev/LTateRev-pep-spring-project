9c54024aa2429708c6608ac146cb56c4a55cb5d2
Leon Tate <throwawayltate@gmail.com>
2025-06-11 18:27:33 +0000

    Completed Account related tests

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,17 @@
 package com.example.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import com.example.service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +19,35 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
 
+    @Autowired
+    AccountService accSer;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<Account> login(@RequestBody Account acc){
+        Account loginAttempt = accSer.tryLogin(acc.getUsername(), acc.getPassword());
+        if (loginAttempt != null){
+            return ResponseEntity.ok(loginAttempt);
+        } else {
+            return ResponseEntity.status(401).build();
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Account> register(@RequestBody Account newAcc ){
+        if (newAcc.getUsername() == null){
+            return ResponseEntity.status(400).build();
+        } else if (newAcc.getPassword() == null || newAcc.getPassword().length() < 4){
+            return ResponseEntity.status(400).build();
+        } else if (accSer.findByUsername(newAcc.getUsername()) != null){
+            return ResponseEntity.status(409).build();
+        } else{
+            Account savedAcc = accSer.registerAccount(newAcc);
+            return ResponseEntity.ok(savedAcc);
+        }
+
+    }
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,25 @@
 package com.example.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.entity.Account;
+
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    @Autowired
+    AccountRepository accRep;
+
+    public Account tryLogin(String user, String pass){
+        return accRep.findByUsernameAndPassword(user, pass);
+    }
+
+    public Account findByUsername(String username){
+        return accRep.findByUsername(username);
+    }
+
+    public Account registerAccount( Account acc){
+        return accRep.save(acc);
+    }
 }
